6bd873cb2372daf0eb01016a2189255f0672d8a9
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2022-09-12 14:44:03 -0300

    Desafio 06 - Ignite NodeJS

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,37 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: {
+        id: user_id
+      }
+    })
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query('SELECT * FROM users AS u WHERE u.first_name ILIKE LOWER($1) AND u.last_name ILIKE LOWER($2)', [first_name, last_name]);
+  }
+}
